c9e9dffee2c2e1abcdab37278dfbbb7f7aa7a532
duddns6290 <duddns6290@naver.com>
2025-10-01 14:38:42 +0900

    Initial commit

---
+++ src/main/java/com/org/cleaningapp_daon/CleaningAppDaonApplication.java
@@ -0,0 +1,13 @@
+package com.org.cleaningapp_daon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningAppDaonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningAppDaonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/cleaningapp_daon/CleaningAppDaonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.cleaningapp_daon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningAppDaonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
